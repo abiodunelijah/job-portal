5841b3b6fb5fbf44e8b82ea57339c7d63416437c
Abiodun Elijah, Siyanbola <43834958+abiodunelijah@users.noreply.github.com>
2025-08-09 10:15:28 +0100

    Add files via upload

---
+++ job-portal/src/main/java/com/coder2client/JobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.coder2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApplication.class, args);
+	}
+
+}
